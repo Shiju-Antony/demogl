ec58396b19688d848f247e3d2f6d16836940568a
Shiju-Antony <2232j35@kgcas.com>
2024-11-13 14:15:14 +0530

    gl demo test

---
+++ wp-content/plugins/surecart/dist/admin/products.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-notices', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '624d932141cd440f1258');
---
+++ wp-content/plugins/surecart/dist/admin/subscriptions/edit.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill', 'wp-url'), 'version' => 'd0a6645b41f5da5388f0');
